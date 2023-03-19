1d5f9556dd8a183834da4e5f8b0d0bbe5af0cf4e
Maximiliano <maximilianogalindo7@gmail.com>
2023-03-18 23:04:43 -0600

    almost done

--- sketch.js
+++ sketch.js
@@ -9,26 +9,49 @@ function setup()
 function draw()
 {
     background(0);
-    translate(200, 200); //where to begin to draw
-
-    let radius = 50;
-
-    stroke(225);
-    noFill();
-    ellipse(0, 0, radius*2);
-
+    translate(185, 200); //where to begin to draw
+    let x = 0;
+    let y = 0;
+    
     //create the point in moving
-    let x = radius*cos(time);
-    let y = radius*sin(time);
-    wave.push(y)
-    fill(225)
-    ellipse(x,y,8); //tiny circle 
-    line(0,0,x,y);
+    for(let i = 0;i < 5;i++){
+        
+        let prevx = x;
+        let prevy = y;
+        
+        let n = i*2 + 1;
+        let radius = 80*(4/(n*PI)); //radius of each circle
 
+        x += radius*cos(n*time);
+        y += radius*sin(n*time);
+        
+        stroke(225, 100);
+        noFill();
+        ellipse(prevx, prevy, radius*2); 
+
+
+        fill(225)
+        stroke(225)
+        line(prevx,prevy,x,y); //lines of each circle
+        ellipse(x,y,8); //each tiny point 
+     }
+
+    wave.unshift(y);
+    line(x,y,185,y);
+
+    beginShape();
+    translate(185,0)
+    
+    noFill()
     for (let i = 0; i < wave.length; i++)
     {
-        point(i, wave[i]);
+        vertex(i, wave[i]);
     }
+    endShape();
+    time += 0.02;
 
-    time += 0.05;
+    if (wave.length > 250){
+        wave.pop();
+    }
 }
+0
