593f3602a9ce58d7eae033eaa715e193cc1a10dc
Maximiliano <maximilianogalindo7@gmail.com>
2023-03-18 19:36:09 -0600

    circle created

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
+let time = 0;
+
 function setup()
 {
-    createCanvas(400,400);
+    createCanvas(600,400);
 }
 
 function draw()
 {
     background(0);
-}
+    translate(200, 200); //relocate the center of the circle
 
-console.log("Hola")
+    let radius = 50;
+
+    stroke(225);
+    noFill();
+    ellipse(0, 0, radius*2);
+
+    time += 0.01;
+}
